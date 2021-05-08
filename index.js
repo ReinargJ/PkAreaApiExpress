11e383d2a670386b025c11e10b984aeee3cdeec1
Julien <julien.granier73@gmail.com>
2021-05-08 15:35:13 +0200

    use database

--- index.js
+++ index.js
@@ -1,42 +1,42 @@
 const express = require('express')
 const fs = require('fs')
-const csv = require('csvtojson')
+const mariadb = require('mariadb')
 var cors = require('cors')
 const app = express()
 
-
-
-// const parseCsv = function (file, complete, error){
-//     papa.parse(file, {worker: true,complete, error})
-// }
-
+const pool = mariadb.createPool({
+  host: '127.0.0.1',
+  user: 'root',
+  password: '',
+  connectionLimit: 5,
+  database: 'pkarea',
+  connectTimeout: 1000
+});
 
 let corsOptions = {
   origin: 'http://127.0.0.1:8081',
   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
 }
 
-app.get('/pkarea',cors(corsOptions), function (req, res) {
+app.get('/pkarea', cors(corsOptions), async function (req, res) {
+  fetchPks(res)
+});
 
-    let data = []
-    csv({delimiter: ';'})
-    .fromFile('/pkarea/resources/database.csv')
-    .on('json',(pk)=>{
+async function fetchPks(res) {
+  let conn;
+  try {
+    console.log("connecting");
+    conn = await pool.getConnection();
+    console.log("connected");
+    let rows = await conn.query("SELECT * from pk");
+    res.send(rows)
+  } catch (err) {
+    res.send(err)
+  } finally {
+    if (conn) return conn.end();
+  }
+}
 
-        pk.pk_fin = pk.pk_fin == "null"? null:pk.pk_fin;
-       data.push(pk);
-    })
-    .on('done',(error)=>{
-        if(error){
-          console.log(error);
-          res.send({})
-        }
-        res.send(data);
-    })  
-
-  });
-  
-
-  app.listen(3000, function () {
-    console.log('Runnning')
-  })
+app.listen(3000, function () {
+  console.log('Runnning')
+})
